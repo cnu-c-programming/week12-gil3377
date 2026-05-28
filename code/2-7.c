38eb3f86e87808bcc0868c18d4e8be8e60709cf8
gil3377 <mungilj3@gmail.com>
2026-05-28 18:51:25 +0900

    Update 2-7.c

--- code/2-7.c
+++ code/2-7.c
@@ -10,18 +10,26 @@ typedef struct config {
 } Config;
 
 void config_parser(Config* config_ptr) {
-
+    FILE* fp = fopen("config.txt", "r");
+    if (fp == NULL) return;
+    char key[64], value[64];
+    while (fscanf(fp, " %63[^=]=%63s", key, value) == 2) {
+        if (strcmp(key, "InputFileName") == 0) strcpy(config_ptr->InputFileName, value);
+        else if (strcmp(key, "Options") == 0) config_ptr->Options = atoi(value);
+        else if (strcmp(key, "SectionName") == 0) strcpy(config_ptr->SectionName, value);
+        else if (strcmp(key, "Address") == 0) config_ptr->Address = strtoull(value, NULL, 16);
+    }
+    fclose(fp);
 }
 
 int main(int argc, const char* argv[]) {
     Config config;
     config_parser(&config);
 
-    printf("config: %s %d %s %llu\n", 
-        config.InputFileName, 
+    printf("config: %s %d %s %llu\n",
+        config.InputFileName,
         config.Options,
         config.SectionName,
         config.Address);
     return 0;
 }
-
