9778066dd6aa19962da3dc91f36e4021f3ba12a6
gil3377 <mungilj3@gmail.com>
2026-05-28 18:50:32 +0900

    Update 2-4.c

--- code/2-4.c
+++ code/2-4.c
@@ -1,13 +1,23 @@
 #include <stdio.h>
 
 int main(int argc, const char* argv[]) {
-    if (argc < 2)
+    if (argc < 2) {
         return 0;
-
+    }
+    
     FILE* fp = fopen(argv[1], "r");
 
+    int counts[26] = {0};
+    int ch;
 
-
+    while ((ch = fgetc(fp)) != EOF) {
+        if (ch >= 'a' && ch <= 'z') {
+            counts[ch - 'a']++;
+        }
+    }
+    for (int i = 0; i < 26; i++) {
+        printf("%c: %d\n", 'a' + i, counts[i]);
+    }
     fclose(fp);
 }
 
