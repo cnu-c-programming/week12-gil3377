13666348310b0b20176d52d83a66f6878bb441a3
gil3377 <mungilj3@gmail.com>
2026-05-28 18:53:23 +0900

    Update 3-5.c

--- code/3-5.c
+++ code/3-5.c
@@ -7,11 +7,18 @@ int main(int argc, const char* argv[]) {
     if (fp == NULL) {
         return 0;
     }
-
-
+    char line[256];
     int sum = 0;
-
-
+    while (fgets(line, sizeof(line), fp) != NULL) {
+        int val;
+        if (sscanf(line, "%d", &val) == 1) {
+            sum += val;
+        }
+        else {
+            line[strcspn(line, "\r\n")] = '\0';
+            fprintf(stderr, "invalid input %s\n", line);
+        }
+    }
     printf("sum: %d\n", sum);
     fclose(fp);
     return 0;
