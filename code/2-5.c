bc728ba2d9a66fd5fe9acf07ed44cee43c269178
gil3377 <mungilj3@gmail.com>
2026-05-28 18:50:47 +0900

    Update 2-5.c

--- code/2-5.c
+++ code/2-5.c
@@ -4,6 +4,15 @@ int main(int argc, const char* argv[]) {
     if (argc < 3)
         return 0;
 
+    FILE* fp_src = fopen(argv[1], "r");
+    FILE* fp_dst = fopen(argv[2], "w");
 
+    char buffer[1024];
+    while (fgets(buffer, sizeof(buffer), fp_src) != NULL) {
+        fputs(buffer, fp_dst);
+    }
+
+    fclose(fp_src);
+    fclose(fp_dst);
 }
 
